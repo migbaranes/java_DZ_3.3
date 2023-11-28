9c73dabfd1d24a491e99df1a0103ab84fecd8db2
migbaranes <migbaranes@gmail.com>
2023-11-28 19:05:50 +0300

    Added percent

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -4,14 +4,17 @@ public class CreditPaymentService {
     // S - сумма кредита;
     // P - размер годовой процентной ставки;
     // N - количество месяцев, в течении которых проводятся выплаты
-    public int calculate(int summCredit, double monthlyInterestRate, int creditTerm) {
+    public int calculate(int summCredit, double annualInterestRate, int creditTerm) {
 
         // double Se;
         double annuityPayment;
         // Se = S * P / 12 * (Math.pow((1 + P / 12), N)) / (Math.pow((1 + P / 12), N-1));
-        annuityPayment = summCredit * ((monthlyInterestRate/12) * (Math.pow((1 + monthlyInterestRate/12), creditTerm))) / (Math.pow((1 + monthlyInterestRate/12), creditTerm) - 1);
-        // Х = С * К, К = (М * (1 + М) ^ S) / ((1 + М) ^ S — 1)
-        // M = Mc / 12
+        annuityPayment = summCredit * ((annualInterestRate / 12 /100 *
+                Math.pow((1 + annualInterestRate / 12/100), creditTerm)) /
+                (Math.pow((1 + annualInterestRate / 12/100), creditTerm) - 1));
+
+        // Х = С * К, К = М * (1 + М) ^ S / ((1 + М) ^ S — 1)
+        // M = Mc / 12 - годовая процентная ставка по кредиту,
         // где, annuity payment — аннуитетный платеж,
         //     summCredit — сумма кредита,
         //     К — коэффициент аннуитета,
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        System.out.println("Ежемесячный платеж при условии займа на 1 год составляет:  " + (service.calculate(1_000_000, 9.99, 12)));
+
+        System.out.println("Ежемесячный платеж при условии займа на 12 месяцев составляет:  " + (service.calculate(1_000_000, 9.99, 12)));
         System.out.println();
         System.out.println("Ежемесячный платеж при условии займа на 2 года составляет:  " +(service.calculate (1_000_000, 9.99, 24)));
         System.out.println();
