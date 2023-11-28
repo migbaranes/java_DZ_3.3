77d323fce614805c5d232c2e0f7c93100eba48fc
migbaranes <migbaranes@gmail.com>
2023-11-28 19:15:28 +0300

    Added percent new

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -9,9 +9,9 @@ public class CreditPaymentService {
         // double Se;
         double annuityPayment;
         // Se = S * P / 12 * (Math.pow((1 + P / 12), N)) / (Math.pow((1 + P / 12), N-1));
-        annuityPayment = summCredit * ((annualInterestRate / 12 /100 *
-                Math.pow((1 + annualInterestRate / 12/100), creditTerm)) /
-                (Math.pow((1 + annualInterestRate / 12/100), creditTerm) - 1));
+        annuityPayment = summCredit * ((annualInterestRate / 12 / 100 *
+                Math.pow((1 + annualInterestRate / 12 / 100), creditTerm)) /
+                (Math.pow((1 + annualInterestRate / 12 / 100), creditTerm) - 1));
 
         // Х = С * К, К = М * (1 + М) ^ S / ((1 + М) ^ S — 1)
         // M = Mc / 12 - годовая процентная ставка по кредиту,
